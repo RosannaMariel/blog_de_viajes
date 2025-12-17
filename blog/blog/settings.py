e224b8b1405a55d4ea618e0d0b4aba5a53db418c
rosannamariel <rosannamariel39@gmail.com>
2025-12-17 00:26:08 -0300

    fix: update database password for MySQL connection

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -92,7 +92,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.mysql",
         "NAME": "blog_viajes",
         "USER": "root",
-        "PASSWORD": "Nico2002!",
+        "PASSWORD": "root",
         "HOST": "localhost",
         "PORT": "3306",
     }
